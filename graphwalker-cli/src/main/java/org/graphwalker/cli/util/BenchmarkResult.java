252c1cf1e9bf676986fe6715c4e45eb6f115eaa7
MaximGonnissen <33728489+MaximGonnissen@users.noreply.github.com>
2024-05-27 00:49:28 +0200

    Store visits in Benchmark results

--- graphwalker-cli/src/main/java/org/graphwalker/cli/util/BenchmarkResult.java
+++ graphwalker-cli/src/main/java/org/graphwalker/cli/util/BenchmarkResult.java
@@ -2,8 +2,11 @@ package org.graphwalker.cli.util;
 
 import com.google.gson.JsonObject;
 import org.graphwalker.core.generator.PathGenerator;
+import org.graphwalker.core.machine.Context;
+import org.graphwalker.core.model.Element;
 import org.graphwalker.core.statistics.Profiler;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class BenchmarkResult {
@@ -13,6 +16,8 @@ public class BenchmarkResult {
   public long testSuiteSize;
   public int identifier;
   public String group;
+  public Map<String, Long> VertexVisits = new java.util.HashMap<>();
+  public Map<String, Long> EdgeVisits = new java.util.HashMap<>();
 
   public BenchmarkResult(int identifier, String group, String path, Profiler profiler, PathGenerator<?> pathGenerator, long seed) {
     this.identifier = identifier;
@@ -21,16 +26,32 @@ public class BenchmarkResult {
     this.seed = seed;
     this.generationTime = profiler.getTotalExecutionTime(TimeUnit.MICROSECONDS);
     this.testSuiteSize = profiler.getTotalVisitCount();
+    calculateVisits(profiler, pathGenerator.getContext());
+  }
+
+  private long getElementVisitCount(Profiler profiler, Context context, Element element) {
+    return profiler.getVisitCount(context, element);
+  }
+
+  private void calculateVisits(Profiler profiler, Context context) {
+    calculateVertexVisits(profiler, context);
+    calculateEdgeVisits(profiler, context);
+  }
+
+  private void calculateVertexVisits(Profiler profiler, Context context) {
+    for (Element element : context.getModel().getVertices()) {
+      VertexVisits.put(element.getName(), getElementVisitCount(profiler, context, element));
+    }
+  }
+
+  private void calculateEdgeVisits(Profiler profiler, Context context) {
+    for (Element element : context.getModel().getEdges()) {
+      EdgeVisits.put(element.getName(), getElementVisitCount(profiler, context, element));
+    }
   }
 
   public String toString() {
-    return "BenchmarkResult{" +
-      "identifier='" + identifier +
-      ", group='" + group +
-      ", seed=" + seed +
-      ", generationTime=" + generationTime +
-      ", testSuiteSize=" + testSuiteSize +
-      '}';
+    return "BenchmarkResult{" + "identifier='" + identifier + ", group='" + group + ", seed=" + seed + ", generationTime=" + generationTime + ", testSuiteSize=" + testSuiteSize + '}';
   }
 
   public JsonObject toJson() {
